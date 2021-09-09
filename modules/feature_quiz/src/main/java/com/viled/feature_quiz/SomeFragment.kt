2ab4b91df2eb4aaf53afc303de1e62908ccaa453
n.nabiyev <n.nabiyev@vlife.kz>
2021-09-09 15:04:20 +0600

    [ADDED] MultiModule fix

--- modules/feature_quiz/src/main/java/com/viled/feature_quiz/SomeFragment.kt
+++ modules/feature_quiz/src/main/java/com/viled/feature_quiz/SomeFragment.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.viled.navigation.NavigationFlow
+import com.viled.navigation.ToFlowNavigatable
 import kotlinx.android.synthetic.main.fragment_some.*
 
 class SomeFragment : Fragment(R.layout.fragment_some) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        button.setOnClickListener {
+        questionsButton.setOnClickListener {
 //            (requireActivity() as ToFlowNavigatable).navigateToFlow(NavigationFlow.QuizFlow)
             findNavController().navigate(R.id.action_someFragment_to_quizFragment)
+        }
+
+        profileButton.setOnClickListener {
+//            findNavController().navigate(R.id.action_someFragment_to_quizFragment)
+                        (requireActivity() as ToFlowNavigatable).navigateToFlow(NavigationFlow.ProfileFlow)
+
         }
     }
 }
